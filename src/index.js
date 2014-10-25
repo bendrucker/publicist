869a0da67fb66d213140d5cb75b2a6abcdfc290e
Ben Drucker <bvdrucker@gmail.com>
2014-10-25 14:24:45 -0400

    Write git commands to release

--- src/index.js
+++ src/index.js
@@ -7,10 +7,17 @@ var semver    = require('semver');
 var Promise   = require('bluebird');
 var path      = require('path');
 var fs        = Promise.promisifyAll(require('fs'));
+var child     = Promise.promisifyAll(require('child_process'));
+
+Promise.longStackTraces();
 
 function fail (err) {
   console.error(err.stack);
   process.exit(1);
+}
+
+function git () {
+  return child.execFileAsync('git', [].slice.apply(arguments));
 }
 
 commander
@@ -22,22 +29,70 @@ commander
       'package.json',
       'bower.json'
     ])
-    .map(function (pkg) {
+    .bind({
+      paths: []
+    })
+    .map(function (pkg, i) {
       var p = path.join(cwd, pkg);
+      this.paths[i] = p;
       return Promise.props({
         path: p,
         data: fs.readFileAsync(p)
       });
     })
-    .map(function (pkg) {
+    .each(function (pkg) {
       pkg.data = JSON.parse(pkg.data);
+    })
+    .tap(function (pkgs) {
+      // store some attributes from the package for later user
+      var pkg = pkgs[0].data;
+      this.main = pkg.main;
+      this.name = pkg.name;
       if (!semver.valid(version)) {
-        version = semver.inc(pkg.data.version, version);
+        version = semver.inc(pkg.version, version);
         if (!version) throw new Error('Invalid version or increment type');
       }
+    })
+    .each(function (pkg) {
       pkg.data.version = version;
       pkg.data = JSON.stringify(pkg.data, void 0, 2);
       return fs.writeFileAsync(pkg.path, pkg.data + '\n');
+    })
+    .then(function () {
+      return git('fetch');
+    })
+    .then(function () {
+      return git('checkout', 'release');
+    })
+    .catch(function (err) {
+      return /pathspec 'release' did not match/.test(err.message);
+    }, function () {
+      return git('checkout', '-b', 'release');
+    })
+    .then(function () {
+      if (!fs.existsSync('./release')) {
+        return fs.mkdirAsync('./release');
+      }
+    })
+    .then(function () {
+      return child.execFileAsync('npm', ['bin'])
+        .spread(function (stdout) {
+          return stdout.replace(/\n$/, '')
+        });
+    })
+    .then(function (binDir) {
+      var releasePath = './release/' + this.name + '.js';
+      this.paths.push(releasePath);
+      return child.execFileAsync(binDir + '/browserify', ['-s', this.name, '-e', this.main, '-o', releasePath]);
+    })
+    .then(function () {
+      return git.apply(null, ['add'].concat(this.paths));
+    })
+    .then(function () {
+      return git('commit', '-m', 'Release v' + version);
+    })
+    .then(function () {
+      return git('tag', 'v' + version);
     })
     .catch(fail);
   });
