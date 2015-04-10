a2bf3c233a9cf280ea8d7f8b64bfd657775a2bb2
Ben Drucker <bvdrucker@gmail.com>
2015-04-09 21:50:10 -0400

    Initial implementation of build pipeline

---
+++ src/build.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import Promise from 'bluebird';
+
+export default function (pack, config = {}) {
+  return Promise.resolve(parse(config))
+    .map(buildDefaults)
+    .map((build) => {
+      return [require(build.package), build.config];
+    })
+    .map(([plugin, config]) => {
+      return [pluginAPI(plugin), plugin.defaults(pack, config)];
+    })
+    .map(([plugin, config]) => {
+      return plugin.before(pack, config).return([plugin, config]);
+    })
+    .map(([plugin, config]) => {
+      return plugin.build(pack, config).return([plugin, config]);
+    })
+    .map(([plugin, config]) => {
+      return plugin.after(pack, config);
+    })
+    .then(() => pack.write());
+}
+
+function parse (config) {
+  if (Array.isArray(config)) return config;
+  return Object.keys(config)
+    .map(function (name) {
+      return {
+        name,
+        config: configDefaults(name, config[name])
+      };
+    });
+}
+
+function buildDefaults (build) {
+  return Object.assign({
+    package: `publicist-${build.name}`
+  }, build);
+}
+
+function configDefaults (name, config) {
+  return Object.assign({
+    dest: `./release/${name}`
+  }, config);
+}
+
+function pluginAPI (plugin) {
+  return Object.assign({}, plugin, {
+    before: noop,
+    build: noop,
+    after: noop
+  });
+}
+
+
+function noop () {}
