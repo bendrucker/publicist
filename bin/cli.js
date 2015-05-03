c72125092da15a45665eac796e52e7e1aec94df4
Ben Drucker <bvdrucker@gmail.com>
2015-05-02 20:29:45 -0400

    Update yargs usage for CLI

--- bin/cli.js
+++ bin/cli.js
@@ -9,7 +9,7 @@ import {cwd} from 'process'
 import {interopRequireWildcard} from 'babel-runtime/helpers'
 import pkg from './package.json'
 
-const {publish, logger} = interopRequireWildcard(require(resolveSync(pkg.name, {
+const publicist = interopRequireWildcard(require(resolveSync(pkg.name, {
   basedir: cwd()
 })))
 
@@ -18,11 +18,21 @@ const argv = yargs
   .example('$0 patch', 'release a new patch version')
   .argv
 
-const [version] = argv._
+const [command] = yargs
+  .usage('$0 command || version')
+  .command('release', 'generate and tag builds')
+  .demand(1, 'must provide a command')
+  .argv
+  ._
 
-publicist
-  .release(version)
-  .catch(fail)
+if (command === 'release') {
+  yargs
+    .demand(2, 'must provide a version')
+    .usage('$0 release <version|increment>')
+    .example('$0 release patch', 'release a new patch version')
+
+  return publicist.release(yargs.argv._[1]).catch(fail)
+}
 
 function fail (err) {
   publicist.logger.error(chalk.red('Release failed'))
